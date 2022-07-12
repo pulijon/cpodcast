a315535c1c8aa06cb8392661f84f69e76923f100
José Miguel Robles <jmrobles@alaveradeviriato.net>
2022-07-12 19:59:31 +0200

    Simplifying the calculations of tracks. There was no need to maintain two variables to store the number of tracks, as it can be obtained from the name of the file.

--- cut.bash
+++ cut.bash
@@ -1,38 +1,46 @@
 #! /bin/bash
-devdir=$(dirname $0)
+#
+# Script to cut mp3 files in fragments of specified sizes
+#
+# Author: José Miguel Robles
+#
+# Date: Julio 2022
+# 
+# Use:
+# cut.bash <file> <seconds> <album> <directory> <short description>
+#
+# Description:
+# Cut the mp3 <file> in fragments of <seconds> size, tagging the results as tracks
+# of the album <album> and copy them in <directory> that is intended to be a
+# folder of a removable storage (mp3 pod, cd, etc.) that allows sequential playing
+# All the tracks include a voice that informs the number of track and 
 
+
+# Cut or file in fragments
 ffmpeg -i $1 -f segment -segment_time $2 -segment_start_number 1 -c copy ${3}_%03d.mp3
-i=1
-numtracks=$(ls ${3}_???.mp3 | wc -l)
+
+numtracks=$(printf "%03d" $(ls ${3}_???.mp3 | wc -l))
 
 for track in ${3}_???.mp3
 do
-	if [ -z $5 ]
-	then
-	    concat_str="concat:$devdir/corte_$i.mp3|$devdir/de_$numtracks.mp3|$track"
-	else
-		gtts-cli "Corte $i de $numtracks. $5 " -l es --output pre_$track
-		concat_str="concat:pre_$track|$track"
-	fi
+	ntrack=$(echo $track | awk '{ sub(/.*_/,""); sub(/\.mp3/,""); print }')
+	gtts-cli "Corte $((10#$ntrack)) de $((10#$numtracks)). $5 " -l es --output pre_$track
+    concat_str="concat:pre_$track|$track"
 	ffmpeg -i "$concat_str" \
 		-c:a libmp3lame \
 		-ar 44100 \
 		-ac 2 \
 		T_$track 
-		# -i $track
-		# -map_metadata 0:1
-	ntrack=$(printf "%03d" $i)
+	echo $ntrack/$numtracks
 	id3tool -a $3 \
-	        -c "$ntrack/$numtracks" \
+	        -c "10#$ntrack" \
 			-t "Corte $ntrack de $numtracks" \
 			T_$track
 	sync T_$track
-	echo $i/$numtracks
 	if [ ! -z $4 ] && [ -d $4 ]
 	then
-		echo Copiando T_$track a $4
+		echo Copying T_$track a $4
 		cp T_$track $4
 		sync $4/T_$track
 	fi
-	((i+=1))
 done
