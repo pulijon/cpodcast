208478971edcd2ef91eec994d1896844cd7d8d46
José Miguel Robles <jmrobles@alaveradeviriato.net>
2022-07-13 10:23:36 +0200

    Adding comments

--- cutpodcast.bash
+++ cutpodcast.bash
@@ -4,38 +4,49 @@
 #
 # Author: José Miguel Robles
 #
-# Date: Julio 2022
+# Date: July, 2022
 # 
 # Use:
-# cut.bash <file> <seconds> <album> <directory> <short description>
+# cut.bash <file> <seconds> <album> <directory> <text>
 #
 # Description:
 # Cut the mp3 <file> in fragments of <seconds> size, tagging the results as tracks
 # of the album <album> and copy them in <directory> that is intended to be a
 # folder of a removable storage (mp3 pod, cd, etc.) that allows sequential playing
-# All the tracks include a voice that informs the number of track and 
+# All the tracks include a voice that informs the number of track and <text>
+# to identify by an audio prefix the fragment
 
 
 # Cut or file in fragments
 ffmpeg -i $1 -f segment -segment_time $2 -segment_start_number 1 -c copy ${3}_%03d.mp3
 
+# Obtain the number of fragments
 numtracks=$(printf "%03d" $(ls ${3}_???.mp3 | wc -l))
 
 for track in ${3}_???.mp3
 do
+	# For each fragment
+	# Obtaining number of track
 	ntrack=$(echo $track | awk '{ sub(/.*_/,""); sub(/\.mp3/,""); print }')
+
+	# Obtaining the speech prefix using the Google service
 	gtts-cli "Corte $((10#$ntrack)) de $((10#$numtracks)). $5 " -l es --output pre_$track
-    concat_str="concat:pre_$track|$track"
+    
+	# Aggregating the prefix to the fragment
+	concat_str="concat:pre_$track|$track"
 	ffmpeg -i "$concat_str" \
 		-c:a libmp3lame \
 		-ar 44100 \
 		-ac 2 \
 		T_$track 
-	echo $ntrack/$numtracks
+	
+	# Tagging the fragment
 	id3tool -a $3 \
 	        -c "10#$ntrack" \
 			-t "Corte $ntrack de $numtracks" \
 			T_$track
+	
+	# Copying to directory avoiding buffer optimizations of OS that could interfere with the order
 	sync T_$track
 	if [ ! -z $4 ] && [ -d $4 ]
 	then
